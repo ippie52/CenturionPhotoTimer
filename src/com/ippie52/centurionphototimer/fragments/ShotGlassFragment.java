9196f70d31e5be55fddf60f87c3c3887c9c9acf1
Kris Dunning <ippie52@hotmail.com>
2013-08-03 22:25:30 +0100

    Update the ShotGlassFragment
    * Update layout of the ShotGlassFragment
      - Removed the button
      - Removed the LinearLayout container
    * Remove usage of button
    * Add updateLevel method to update the level of the shot glass
      based on a percentage

--- src/com/ippie52/centurionphototimer/fragments/ShotGlassFragment.java
+++ src/com/ippie52/centurionphototimer/fragments/ShotGlassFragment.java
@@ -1,14 +1,10 @@
 package com.ippie52.centurionphototimer.fragments;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.ippie52.centurionphototimer.R;
 import com.ippie52.centurionphototimer.utils.FragmentFrame;
@@ -30,28 +26,6 @@ public class ShotGlassFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * onAttach - Method checks that activity being attached implements the
-     * {@link OnShotGlassFragmentListener} interface
-     * **/
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnShotGlassFragmentListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnShotGlassFragmentListener");
-        }
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
@@ -66,61 +40,22 @@ public class ShotGlassFragment extends Fragment {
         if (mGlass != null) {
             mGlass.setProgressPixels(0);
         }
-        Button b = (Button) v.findViewById(R.id.shot_glass_update_value_btn);
-        if (b != null) {
-            b.setText("Click me!");
-            b.setOnClickListener(new OnClickListener() {
 
-                @Override
-                public void onClick(View arg0) {
-                    if (mGoingUp) {
-                        int delta = ((mProgress + mGlass.getMaxPixels()) / 2)
-                                - mProgress;
-                        mProgress += delta == 0 ? 1 : delta;
-
-                        if (mProgress == mGlass.getMaxPixels()) {
-                            mGoingUp = !mGoingUp;
-                        }
-
-                    } else {
-                        mProgress /= 2;
-
-                        if (mProgress == 0) {
-                            mGoingUp = !mGoingUp;
-                        }
-
-                    }
-                    mGlass.setProgressPixels(mProgress);
-                }
-            });
-        }
-        // Inflate the layout for this fragment
         return v;
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated to
-     * the activity and potentially other fragments contained in that activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnShotGlassFragmentListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+     * updateLevel - Method to update the level of the glass to a fill
+     * percentage
+     * 
+     * @param aPercent
+     *            The fill percentage
+     * **/
+    public void updateLevel(final float aPercent) {
+        final int progress = (int) ((mGlass.getMaxPixels() * aPercent) / 100);
+        mGlass.setProgressPixels(progress);
     }
 
     private ShotGlassView mGlass;
-    private OnShotGlassFragmentListener mListener;
-    private int mProgress = 0;
-    private boolean mGoingUp = true;
 
 }
