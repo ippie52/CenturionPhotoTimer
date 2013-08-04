45ded073668e907338bb8ed49e2093bd73973736
Kris Dunning <ippie52@hotmail.com>
2013-08-04 23:03:34 +0100

    Update TimeToNextFragment
    Update the "time to next" fragment to show the time to next
    currently with the number of seconds, then milliseconds.
    
    TODO: Perhaps drop the accuracy of values shown

--- src/com/ippie52/centurionphototimer/fragments/TimeToNextFragment.java
+++ src/com/ippie52/centurionphototimer/fragments/TimeToNextFragment.java
@@ -1,14 +1,19 @@
 package com.ippie52.centurionphototimer.fragments;
 
-import android.app.Activity;
-import android.net.Uri;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ippie52.centurionphototimer.R;
+import com.ippie52.centurionphototimer.utils.ColourFader;
 import com.ippie52.centurionphototimer.utils.FragmentFrame;
 
 /**
@@ -20,28 +25,26 @@ import com.ippie52.centurionphototimer.utils.FragmentFrame;
  */
 public class TimeToNextFragment extends Fragment {
 
+    /**
+     * Blank Constructor
+     * **/
     public TimeToNextFragment() {
-        // Required empty public constructor
     }
 
+    /**
+     * Overridden method
+     * 
+     * TODO Fill in the arguments here if needed
+     * **/
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnTimeToNextFragmentListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnTimeToNextFragmentListener");
-        }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
+    /**
+     * Overridden method to create the view for this fragment after setting up
+     * components as necessary
+     * **/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
@@ -52,29 +55,61 @@ public class TimeToNextFragment extends Fragment {
         if (frame != null) {
             frame.setColour(0x34aa4411);
         }
+
+        Bundle args = getArguments();
+        if (args != null) {
+            mMaxTimeToNext = args.getInt(MAX_TIME_TO_NEXT_MILLISECONDS);
+        } else {
+            mMaxTimeToNext = (int) DEFAULT_TIME;
+        }
+
+        mTimeToNextText = (TextView) v
+                .findViewById(R.id.time_to_next_text_view);
+        if (mTimeToNextText != null) {
+            mTimeToNextText.setText(mDf.format(new Date(DEFAULT_TIME)));
+            mTimeToNextText.setTextSize(120f);
+        }
         return v;
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    /**
+     * Method to set the maximum time to be displayed
+     * 
+     * @param aMaxTime
+     *            The maximum time to be displayed in milliseconds
+     * **/
+    public void setMaxTime(final int aMaxTimeMs) {
+        mMaxTimeToNext = aMaxTimeMs;
     }
 
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated to
-     * the activity and potentially other fragments contained in that activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnTimeToNextFragmentListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+     * Method to set the time remaining in milliseconds
+     * 
+     * @param aTimeToNextMs
+     *            The time remaining in milliseconds
+     * **/
+    public void setTimeRemaining(final long aTimeToNextMs) {
+        mTimeToNextText.setText(mDf.format(aTimeToNextMs));
+        float percent = (100f * aTimeToNextMs) / mMaxTimeToNext;
+        mTimeToNextText.setTextColor(mFader.getColour(percent));
     }
 
-    private OnTimeToNextFragmentListener mListener;
+    /**
+     * Class Constants
+     * **/
+    private final static long DEFAULT_TIME = (60 * 1000);
+    public final static String MAX_TIME_TO_NEXT_MILLISECONDS = "com.ippie52.centurionphototimer.timetonextfragment.max-time-to-next-milliseconds";
+    private final static int START_COLOUR = Color.GREEN;
+    private final static int END_COLOUR = Color.RED;
+    private final static ColourFader mFader = new ColourFader(START_COLOUR,
+            END_COLOUR);
+    @SuppressLint("SimpleDateFormat")
+    private final static SimpleDateFormat mDf = new SimpleDateFormat("ss:SSS");
+
+    /**
+     * Member Variables
+     * **/
+    private TextView mTimeToNextText;
+    private int mMaxTimeToNext;
 
 }
